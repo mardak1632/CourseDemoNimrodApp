15e674b457db2b1140acc1f41897e466c823a7c9
Mehmet Adem Sengul <masengul@gmail.com>
2014-02-02 21:17:03 +0200

    initialize commit

---
+++ src/com/bilisimegitim/coursedemo/main/DateUtil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bilisimegitim.coursedemo.main;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Adem
+ */
+public class DateUtil {
+    
+    public static String dateToStr(Date date, String format){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+        return simpleDateFormat.format(date);
+    }
+    
+    public static double bilesikFaizHesapla(double anaPara, int ay){
+        
+        return 0;
+    }
+}
